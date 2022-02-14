d7ec6f7b7d5c6ddc5a541921fc98a3859f612519
jabber <2898117012@qq.com>
2022-02-14 15:53:10 +0800

    fix: test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,7 +25,6 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
     }
 }
